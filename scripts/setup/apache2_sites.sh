86990eaf93bfb8af4be7838d3ceca6ba63a0113c
Derek Hoagland <thegrickit@gmail.com>
2016-05-08 17:30:22 -0400

    Some extra cleanup

--- scripts/setup/apache2_sites.sh
+++ scripts/setup/apache2_sites.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+# add our sites to sites-available
 sudo ln -s /home/www-data/config/apache2_sites/*.conf /etc/apache2/sites-available
+
+# disable the default site
+sudo a2dissite 000-default
+
+# disable the site with the /phpmyadmin alias
+sudo a2disconf phpmyadmin
